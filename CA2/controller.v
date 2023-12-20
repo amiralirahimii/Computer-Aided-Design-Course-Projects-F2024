86df7eb0fc348cfd8bbdc92c0a8067f5190fd26a
amiralirahimii <amiralirahimi82@gmail.com>
2023-12-20 18:51:07 +0330

    controller complete

--- CA2/controller.v
+++ CA2/controller.v
@@ -2,9 +2,6 @@ module controller(clk, start, complete, sel, en0, en1, en2, en3, done);
 	input clk, start, complete;
 	output sel, en0, en1, en2, en3, done;
 	
-	parameter[2:0] Q0=3'b000, Q1=3'b001, Q2=3'b010, Q3=3'b011, Q4=3'b100,
-			Q5=3'b101, Q6=3'b110;
-
 	wire[2:0] ps=3'b0, ns;
     wire[2:0] notPs;
     Not NOTPS2(ps[2], notPs[2]);
@@ -49,34 +46,5 @@ module controller(clk, start, complete, sel, en0, en1, en2, en3, done);
     And #3 AND16({ps[2], notPs[1], ps[0]}, en3);
     And #3 AND17({ps[2], ps[1], notPs[0]}, done);
 
-
-	always@(posedge clk)
-	begin
-		ps<=ns;
-	end
-	always@(ps,start,complete)
-	begin
-		case(ps)
-			Q0 : ns= start ? Q1 : Q0 ;
-			Q1 : ns= Q2 ;
-			Q2 : ns= Q3 ;
-			Q3 : ns= Q4 ;
-			Q4 : ns= Q5 ;
-			Q5 : ns= complete ? Q6 : Q3;
-			Q6 : ns= Q0;
-		endcase
-	end
-	always@(ps,start,complete)
-	begin
-		{sel, en0, en1, en2, en3, done}=6'b000000;
-		case(ps)
-			Q0 : ;
-			Q1 : en0= 1'b1;
-			Q2 : {sel, en1}= 2'b11;
-			Q3 : en2= 1'b1;
-			Q4 : ;
-			Q5 : {sel, en1, en3}= 3'b011;
-			Q6 : done= 1'b1;
-		endcase
-	end
+    register #3 (.clk(clk), .rst(1'b0), .en(1'b1), .in(ns), .out(ps));
 endmodule
