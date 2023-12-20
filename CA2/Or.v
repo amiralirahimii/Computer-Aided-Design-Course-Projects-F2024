17be3a103efe9595ea11a9ce5f0f2ca25429287f
amiralirahimii <amiralirahimi82@gmail.com>
2023-12-20 16:47:08 +0330

    fixed parameterized Or

--- CA2/Or.v
+++ CA2/Or.v
@@ -1,11 +1,11 @@
-module Or #(parameter N = 4)(in, out);
+module Or #(parameter N = 2)(in, out);
     input [N-1:0] in;
     output out;
     wire [N-2:0] or_intermediate;
 
     genvar i;
     generate
-        C1 C1_OR(.A0(in[0]), .A1(1'b1), .SA(in[1]), .B0(),
+        C1 C1_OR_INIT(.A0(in[0]), .A1(1'b1), .SA(in[1]), .B0(),
                     .B1(), .SB(), .S0(1'b0), .S1(1'b0), .F(or_intermediate[0]));
         for (i = 1; i < N-1; i = i + 1)
         begin
