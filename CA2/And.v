e25b67bfa6348ee3a3e9224b24453b940d2f7e4d
soheil <soheilhm1381@gmail.com>
2023-12-20 17:54:21 +0330

    fix and

--- CA2/And.v
+++ CA2/And.v
@@ -1,8 +1,8 @@
-module Or #(parameter N = 2)(in, out);
+module And #(parameter N = 2)(in, out);
     input [N-1:0] in;
     output out;
     wire [N-2:0] and_intermediate;
-
+    wire kossher;
     genvar i;
     generate
         C1 C1_AND_INIT(.A0(1'b0),.A1(in[0]),.SA(in[1]),.B0(),.B1(),.SB(),.S0(1'b0),.S1(1'b0),.F(and_intermediate[0]));
