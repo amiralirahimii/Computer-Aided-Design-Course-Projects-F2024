ab40d58b3d159b767bf55e97c779f8043113180d
soheil <soheilhm1381@gmail.com>
2023-12-20 14:01:34 +0330

    add necessary gates

---
+++ CA2/Xor.v
@@ -0,0 +1,14 @@
+module Xor(a,b,out);
+input a,b;
+wire not_a,not_b;
+wire and1_out,and2_out;
+output out;
+Not NOT_A(a,not_a);
+Not NOT_B(b,not_b);
+
+And AND1(not_a,b,and1_out);
+And AND2(a,not_b,and2_out);
+
+Or OR(and1_out,and2_out,out);
+
+endmodule
